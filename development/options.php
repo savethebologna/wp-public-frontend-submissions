1ff041aaa4fa8d517aad9067241a57247ba5c672
Justin Goreschak <savethebologna@gmail.com>
2014-07-19 23:28:40 -0400

    Forms display, added settings

--- development/options.php
+++ development/options.php
@@ -14,7 +14,7 @@ function create_fef_settings() {
 
 function register_fef_settings() {
 	//register our settings
-	register_setting( 'fef_settings', 'fef_frontendfields' );
+	register_setting( 'fef_settings', 'fef_emailoptions' );
 	register_setting( 'fef_settings', 'fef_publicuser' );
 	register_setting( 'fef_settings', 'fef_fields' );
 }
@@ -24,58 +24,68 @@ function fef_settings_page() {
 	global $publicuser;
 	global $fields;
 ?>
+<style>
+.feffield input{
+	display:none;
+}
+</style>
 <div class="wrap">
 	<h2>Public Frontend Submissions</h2>
-	<div class="center">
-		<div id="tscmod_reorder" class="postbox">
-			<div class="inside">
-			<h3>Options</h3>
-				<form method="post" action="options.php">
-					<p>Customize your form.</p>
-					<label>Public User:</label>
-					<select name="fef_publicuser">
-					<?php
-					foreach( $blogusers as $user ){
-						if($publicuser === $user->ID){ $selected = "selected"; }else{ $selected = ""; }
-						$option = '<option value="' . $user->ID . '" '.$selected.'>';
-						$option .= $user->display_name;
-						$option .= '</option>';
-						echo $option;
+	<div class="center"><div id="tscmod_reorder" class="postbox"><div class="inside">
+		<h2>Options</h2>
+		<form method="post" action="options.php">
+			<p><h3>Form Fields</h3>
+				<?php
+				if( is_array($fields) ){
+					foreach( $fields as $field_id => $field_data ){
+						$label = $field_data[label];
+						$active = "";
+						$required = "";
+						if( $field_data[active] == true ) $active = "checked";
+						if( $field_data[required] == true ) $required = "checked";
+						echo "<div class='feffield'>
+							<span class='feflabel'>
+								".$label." (ID: fef_".$field_id.")
+								<a href='#' class='editField'>Edit Field</a>
+							</span>
+							<input type='checkbox' name='fef_fields[".$field_id."][active]' value='true' ".$active." />Active
+							<input type='checkbox' name='fef_fields[".$field_id."][required]' value='true' ".$required." />Required
+							<input name='fef_fields[".$field_id."][label]' value='".$label."' />
+							</div>\n";
 					}
-					?>
-					</select>
-					<?php
-					if( is_array($fields) ){
-						foreach( $fields as $field_id => $field_data ){
-							$label = $field_data['label'];
-							echo "<div class='feffield'>
-								<span class='feflabel'>
-									".$label." (ID: fef_".$field_id.")
-									<a href='#' class='editField'>Change Label</a>
-								</span>
-								<input name='fef_fields[".$field_id."][label]' value='".$label."' />
-								</div>\n";
-						}
-					}else{
-						echo "\n<p>You don't have any fields yet.</p>";
-					} //FIX BUG: if a new field is added, a blank field entry will still save as a new field.
-					?>
-					<div id="newfields"></div>
-					<p><a href="#" onClick="addField();">Add a field</a></p>
-					<?php settings_fields( 'fef_settings' ); ?>
-					<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes" /></p>
-				</form>
-			</div>
-		</div>
-	</div>
+				}else{
+					echo "\n<p>You don't have any fields yet.</p>";
+				} //FIX BUG: if a new field is added, a blank field entry will still save as a new field.
+				?>
+				<div id="newfields"></div>
+				<p><a href="#" onClick="addField();">Add a field</a></p>
+			</p>
+			<p><h3>More Options</h3>
+				<label>Public User:</label><br>
+				<select name="fef_publicuser">
+				<?php
+				foreach( $blogusers as $user ){
+					if($publicuser === $user->ID){ $selected = "selected"; }else{ $selected = ""; }
+					$option = '<option value="' . $user->ID . '" '.$selected.'>';
+					$option .= $user->display_name;
+					$option .= '</option>';
+					echo $option;
+				}
+				?>
+				</select>
+			</p>
+			<?php settings_fields( 'fef_settings' ); ?>
+			<p class="submit" style="text-align:center"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes" /></p>
+		</form>
+	</div></div></div>
 </div>
 <script>
 	function addField(){
 		event.preventDefault();
 		var fefID = jQuery('.feffield').length;
-		jQuery('#newfields').append('<div class="feffield"><label>New Field:</label><input name="fef_fields[' + fefID + '][label]" value=""></div>');
+		jQuery('#newfields').append('<div class="feffield"><input style="display:block !important" name="fef_fields[' + fefID + '][label]" value="" placeholder="Field Label"></div>');
 	}
-	jQuery( ".label a.editField" ).click(function( event ) {
+	jQuery( ".feflabel a.editField" ).click(function( event ) {
 		event.preventDefault();
 		jQuery( this ).parent().hide();
 		jQuery( this ).parent().siblings('input').show();
--- development/public-frontend-submissions.php
+++ development/public-frontend-submissions.php
@@ -12,151 +12,168 @@
 $dir = dirname( __FILE__ );
 
 //OPTIONS
-$frontendfields = get_option('fef_frontendfields'); //get all the fields set on the options page
 $publicuser = (int) get_option('fef_publicuser'); //get the user id that anonymous submissions will use as author
-$fields = get_option('fef_fields');
+$fields = get_option('fef_fields'); //get all the fields set on the options page
+$emailoptions = get_option('fef_emailoptions'); //grab email options
 
 require( $dir . '/options.php' ); //build admin page for options
 require( $dir . '/public-posts-manager.php' ); //Create custom post type
 
 /*----------------SHORTCODES------------------*/
-add_shortcode( 'frontendform' , 'build_frontend_form' );
+add_shortcode( 'frontendform' , 'frontend_form_shortcode' );
 
-function build_frontend_form( $atts, $content ){
+function frontend_form_shortcode( $atts, $content ){
 	//Set defaults for attributes
 	extract( shortcode_atts( array(
 		'form' => 0,
 	), $atts, 'frontendform' ) );
 	
-if( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['fef-submit'] )) {
-
-	if( is_user_logged_in() ) {
-		$publicuser = get_current_user_id();
-	}elseif( empty($publicuser) ){
-		$publicuser = 0;
-	}
+	$submitform = create_frontend_post();
 	
-	$post = array(
-		'post_title'	=> $title,
-		'post_content'	=> $description,
-		'tags_input'	=> $tags,
-		'post_status'	=> 'pending',
-		'post_author'   => $publicuser,
-		'post_category' => array('1'),
-		'post_type'	=> 'frontend'
-	);
-	$new_post_id = wp_insert_post( $post );
+	if( isset($submitform) ){
+		return $submitform;
+	}else{
+		return build_frontend_form();
+	}
+}
 
-	foreach( $_POST as $fullkey => $response ){ //read off each POST value
-		$response = wp_strip_all_tags( $response );
-		$pos = strpos($fullkey , 'fef'.$post_id.'-'); //keep only the ones that matter to us
-		if( $pos === 0 && $response != '' && !empty( $response ) ){
-			$field = str_replace( 'fef'.$post_name.'-', '', $fullkey ); //fef identifier
+function build_frontend_form(){
+	$fields = get_option('fef_fields'); //shortcode won't use the global variable on the frontend
+	$nofields = "<p style='margin:0;padding:5px 15px 0 15px;border-left:1px solid #dedede;'>This form is empty.</p>";
+	$i = 0;
+	$form = "<form>";
+	if( is_array($fields) ){
+		foreach( $fields as $field_id => $field_data ){
+			if( $field_data[active] == true ){
+				$label = $field_data[label];
+				$required = "";
+				if( $field_data[required] == true ) $required = "required";
+				$form .= "<p style='margin:0;padding:5px 15px 0 15px;border-left:1px solid #dedede;'><label for='enddate'>".$label.": </label><input size='16' name='enddate' placeholder='".$required."' ".$required." /></p>\n";
+				$i++;
+			}
+			if( $i = 0 ) $form .= $nofields;
 		}
-		update_post_meta( $consultant_id, $module, $resultsarray );
+	}else{
+		$form .= $nofields;
 	}
-	$emailto = array( $contactemail, 'submission@ionicevents.com');
-	$subject = "Event Submitted";
-	$message = "Greetings!<p>Thank you for submitting an event to Ionic Events. You will be notified when your event is approved. Event: </p><p>Thank you,<br>The Ionic Events Team<p>";
-	
-	if(strpos($hosturl, 'http://') !== false) {
-	$http = 'http://';
-	$hosturl = implode('',array($http,$rawhosturl));
-	} else {
-	$hosturl = $rawhosturl;
-	}
-	
-	// Add the content of the form to $post as an array
-	$post = array(
-		'post_title'	=> $title,
-		'post_content'	=> $description,
-		'tags_input'	=> $tags,
-		'post_status'	=> 'pending',
-		'post_author'   => $user,
-		'post_category' => array('1'),
-		'post_type'	=> 'event'  // Use a custom post type if you want to
-	);
-	$the_post_id = wp_insert_post( $post );
-	__update_post_meta($the_post_id, "street", $_POST["street"]); 
-	__update_post_meta($the_post_id, "city_state", $_POST["city_state"]);
-	__update_post_meta($the_post_id, "feature", $_POST["feature"]);
-	__update_post_meta($the_post_id, "venue", $_POST["venue"]);
-	__update_post_meta($the_post_id, "ubuilding", $_POST["ubuilding"]);
-	__update_post_meta($the_post_id, "cost", $_POST["cost"]);
-	__update_post_meta($the_post_id, "room", $_POST["room"]);
-	__update_post_meta($the_post_id, "startdate", strtotime($startdate));
-	__update_post_meta($the_post_id, "enddate", strtotime($enddate));
-	__update_post_meta($the_post_id, "host", $_POST["host"]);
-	__update_post_meta($the_post_id, "hosturl", $_POST["hosturl"]);
-	__update_post_meta($the_post_id, "contactemail", $contactemail);
-	__update_post_meta($the_post_id, "startdate2", $startdate);
-	__update_post_meta($the_post_id, "enddate2", $enddate);
+	$form .= wp_nonce_field( 'new-post', '_wpnonce', true, true );
+	$form .= "<p style='text-align:center;padding:10px;'><input type='submit' /></p>";
+	$form .= "</form>";
+	return $form;
+}
 
-	// require two files that are included in the wp-admin but not on the front end.  These give you access to some special functions below.
-	require $_SERVER['DOCUMENT_ROOT'] . "/wp-admin/includes/image.php";
+function create_frontend_post(){
+
+	if( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['fef-submit'] )){
+
+		//Form Validation
+		if( $emailoptions[active] === true ){
+			$posteremail = $_POST[$emailoptions['emailfield']];
+			if( $emailoptions['posteremailrequired'] === true && !filter_var( $posteremail, FILTER_VALIDATE_EMAIL ) ){
+				return "A valid email address is required.";
+			}
+		}
+	
+		if( is_user_logged_in() ) {
+			$publicuser = get_current_user_id();
+		}elseif( empty($publicuser) ){
+			$publicuser = 0;
+		}
+		
+		$post = array(
+			'post_title'	=> $title,
+			'post_content'	=> $description,
+			'tags_input'	=> $tags,
+			'post_status'	=> 'pending',
+			'post_author'   => $publicuser,
+			'post_category' => array('1'),
+			'post_type'	=> 'frontend'
+		);
+		$new_post_id = wp_insert_post( $post );
+
+		foreach( $_POST as $fullkey => $response ){ //read off each POST value
+			$response = wp_strip_all_tags( $response );
+			$pos = strpos($fullkey , 'fef'.$post_id.'-'); //keep only the ones that matter to us
+			if( $pos === 0 && $response != '' && !empty( $response ) ){
+				$field = str_replace( 'fef'.$post_name.'-', '', $fullkey ); //fef identifier
+			}
+			update_post_meta( $consultant_id, $module, $resultsarray );
+		}
+		
+		// require two files that are included in the wp-admin but not on the front end.  These give you access to some special functions below.
+		require $_SERVER['DOCUMENT_ROOT'] . "/wp-admin/includes/image.php";
+		 
+		// required for wp_handle_upload() to upload the file
+		$upload_overrides = array( 'test_form' => FALSE );
+		 
+		// count how many files were uploaded
+		$count_files = count( $_FILES['files'] );
+		 
+		// load up a variable with the upload direcotry
+		$uploads = wp_upload_dir();
+		 
+		// foreach file uploaded do the upload
+		foreach ( range( 0, $count_files ) as $i ) {
+			// create an array of the $_FILES for each file
+			$file_array = array(
+					'name'          => $_FILES['files']['name'][$i],
+					'type'          => $_FILES['files']['type'][$i],
+					'tmp_name'      => $_FILES['files']['tmp_name'][$i],
+					'error'         => $_FILES['files']['error'][$i],
+					'size'          => $_FILES['files']['size'][$i],
+			);
 	 
-	// required for wp_handle_upload() to upload the file
-	$upload_overrides = array( 'test_form' => FALSE );
+			// check to see if the file name is not empty
+			if ( !empty( $file_array['name'] ) ) {
+				$fileurl = $uploads['url'] . '/' . basename( $uploaded_file['file'] );
 	 
-	// count how many files were uploaded
-	$count_files = count( $_FILES['files'] );
+					// upload the file to the server
+				$uploaded_file = wp_handle_upload( $file_array, $upload_overrides );
+				$filepath = $uploaded_file['file'];
 	 
-	// load up a variable with the upload direcotry
-	$uploads = wp_upload_dir();
+					// checks the file type and stores in in a variable
+				$wp_filetype = wp_check_filetype( basename( $uploaded_file['file'] ), null );
 	 
-	// foreach file uploaded do the upload
-	foreach ( range( 0, $count_files ) as $i ) {
-        // create an array of the $_FILES for each file
-        $file_array = array(
-                'name'          => $_FILES['files']['name'][$i],
-                'type'          => $_FILES['files']['type'][$i],
-                'tmp_name'      => $_FILES['files']['tmp_name'][$i],
-                'error'         => $_FILES['files']['error'][$i],
-                'size'          => $_FILES['files']['size'][$i],
-        );
- 
-        // check to see if the file name is not empty
-        if ( !empty( $file_array['name'] ) ) {
-			$fileurl = $uploads['url'] . '/' . basename( $uploaded_file['file'] );
- 
-                // upload the file to the server
-            $uploaded_file = wp_handle_upload( $file_array, $upload_overrides );
-			$filepath = $uploaded_file['file'];
- 
-                // checks the file type and stores in in a variable
-            $wp_filetype = wp_check_filetype( basename( $uploaded_file['file'] ), null );
- 
-				// set up the array of arguments for "wp_insert_post();"
-            $attachment = array(
-                'post_mime_type' => $wp_filetype['type'],
-                'post_title' => preg_replace('/.[^.]+$/', '', basename( $uploaded_file['file'] ) ),
-                'post_content' => '',
-                'post_author' => $user,
-                'post_status' => 'inherit',
-                'post_type' => 'attachment',
-                'post_parent' => $the_post_id,
-                'guid' => $fileurl,
-            );
- 
-            // insert the attachment post type and get the ID
-            $attachment_id = wp_insert_attachment( $attachment, $filepath );
- 
-                // generate the attachment metadata
-                $attach_data = wp_generate_attachment_metadata( $attachment_id, $uploaded_file );
- 
-                // update the attachment metadata
-                wp_update_attachment_metadata( $attachment_id,  $attach_data );
- 
-                // this is optional and only if you want to.  it is here for reference only.
-                // you could set up a separate form to give a specific user the ability to change the post thumbnail
-                set_post_thumbnail( $the_post_id, $attachment_id );
- 
-        }
+					// set up the array of arguments for "wp_insert_post();"
+				$attachment = array(
+					'post_mime_type' => $wp_filetype['type'],
+					'post_title' => preg_replace('/.[^.]+$/', '', basename( $uploaded_file['file'] ) ),
+					'post_content' => '',
+					'post_author' => $user,
+					'post_status' => 'inherit',
+					'post_type' => 'attachment',
+					'post_parent' => $the_post_id,
+					'guid' => $fileurl,
+				);
+	 
+				// insert the attachment post type and get the ID
+				$attachment_id = wp_insert_attachment( $attachment, $filepath );
+	 
+					// generate the attachment metadata
+					$attach_data = wp_generate_attachment_metadata( $attachment_id, $uploaded_file );
+	 
+					// update the attachment metadata
+					wp_update_attachment_metadata( $attachment_id,  $attach_data );
+	 
+					// this is optional and only if you want to.  it is here for reference only.
+					// you could set up a separate form to give a specific user the ability to change the post thumbnail
+					set_post_thumbnail( $the_post_id, $attachment_id );
+	 
+			}
+		}
+		
+		if($emailoptions[active] === true){
+			if( $emailoptions['sendtouser'] === true && !empty($posteremail) ) $emailto[] = $posteremail;
+			$emailto[] = $emailoptions['autoemail'];
+			$subject = $emailoptions['subject'];
+			$message = $emailoptions['message']; //html enabled, no variables yet though
+			wp_mail($mailto,$subject,$message);
+		}
+		
+		return "success";
 	}
-	wp_mail($mailto,$subject,$message);
+	
 }
-}
-   
 // Do the wp_insert_post action to insert it
 do_action('wp_insert_post', 'wp_insert_post');
 ?>
